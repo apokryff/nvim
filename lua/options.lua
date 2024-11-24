543c0e1455230ec3f8898648ad6ff213c1f3e2ae
apokryff <megagugl530@gmail.com>
2024-11-24 14:12:59 +0000

    Add breakepoints icon

--- lua/options.lua
+++ lua/options.lua
@@ -6,6 +6,14 @@ for type, icon in pairs(signs) do
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
+vim.api.nvim_set_hl(0, "blue", { fg = "#3d59a1" })
+vim.api.nvim_set_hl(0, "green", { fg = "#9ece6a" })
+vim.api.nvim_set_hl(0, "yellow", { fg = "#FFFF00" })
+vim.api.nvim_set_hl(0, "orange", { fg = "#f09000" })
+vim.api.nvim_set_hl(0, "red", { fg = "#FF5D62" })
+
+vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "red", linehl = "DapBreakpoint", numhl = "DapBreakpoint" })
+
 -- add yours here!
 
 local o = vim.opt
